751731aef0456f7b399ad0cf3019b2f4690bb806
SRserves85 <srserves85@Scotts-MacBook-Pro.local>
2017-03-11 16:09:23 -0600

    test

---
+++ env.py
@@ -0,0 +1,9 @@
+"""
+This file contains environment-specific variables.
+"""
+import pandas as pd
+
+pd.set_option('display.width', None)
+pd.set_option('display.max_rows', 50)
+pd.set_option('display.max_columns', None)
+pd.options.mode.chained_assignment = None
